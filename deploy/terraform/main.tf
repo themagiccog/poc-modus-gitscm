185c08220c87cfc08a3e57ccf85c653449fbabd8
COG (from Surface) <me@cog.rocks>
2025-06-30 20:02:47 -0500

    Refactor Azure Linux Web App configuration to use application stack for Docker settings

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -39,15 +39,18 @@ resource "azurerm_linux_web_app" "app" {
   service_plan_id     = azurerm_app_service_plan.plan.id
 
   site_config {
-    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/flask-app:latest"
+    container_registry_use_managed_identity = false
+    application_stack {
+      docker_image_name     = "${azurerm_container_registry.acr.login_server}/flask-app:latest"
+      docker_registry_url   = "https://${azurerm_container_registry.acr.login_server}"
+      docker_registry_username = azurerm_container_registry.acr.admin_username
+      docker_registry_password = azurerm_container_registry.acr.admin_password
+    }
   }
 
   app_settings = {
     WEBSITES_PORT = "8000"
     CUSTOM_MESSAGE = "Hello from Terraform"
     DEPLOY_ENV = "terraform"
-    DOCKER_REGISTRY_SERVER_URL      = "https://${azurerm_container_registry.acr.login_server}"
-    DOCKER_REGISTRY_SERVER_USERNAME = azurerm_container_registry.acr.admin_username
-    DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_container_registry.acr.admin_password
   }
 }
