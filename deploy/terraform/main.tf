9161354605a8bba8a81504b4a0d78c6741815778
COG <cog@cog.com>
2025-07-01 20:27:06 -0500

    fix: remove provider reference from azurerm_container_registry data source

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -18,10 +18,10 @@ resource "azurerm_resource_group" "rg" {
 # }
 
 data "azurerm_container_registry" "acr" {
-  provider            = azurerm.infra
+  # provider            = azurerm.infra
   name                = "az4africa"
   resource_group_name = "core-rg"
-  location            = "South Central US"
+
 }
 
 
