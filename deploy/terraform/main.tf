42d86426daf0267add78e42341f2c2bd0e1084c0
COG <cog@cog.com>
2025-07-01 20:33:38 -0500

    fix: comment out linux_fx_version in web app configuration

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -48,7 +48,7 @@ resource "azurerm_linux_web_app" "app" {
   site_config {
     container_registry_use_managed_identity = false
     always_on               = false
-    linux_fx_version = " DOCKER|${data.azurerm_container_registry.acr.login_server}/flask-app:latest"
+    # linux_fx_version = " DOCKER|${data.azurerm_container_registry.acr.login_server}/flask-app:latest"
     application_stack {
       docker_image_name     = "${data.azurerm_container_registry.acr.login_server}/flask-app:latest"
       docker_registry_url   = "https://${data.azurerm_container_registry.acr.login_server}"
