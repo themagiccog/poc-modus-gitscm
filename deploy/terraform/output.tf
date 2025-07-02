7f60c6ac299a6f375d1d4f341d8ad70e5c003930
COG <cog@cog.com>
2025-07-01 23:29:38 -0500

    feat: add output for image URL in Terraform configuration

--- deploy/terraform/output.tf
+++ deploy/terraform/output.tf
@@ -5,3 +5,7 @@ output "acr_login_server" {
 output "webapp_url" {
   value = azurerm_linux_web_app.app.default_hostname
 }
+
+output "image_url" {
+  value = "${data.azurerm_container_registry.acr.login_server}/flask-app:latest"
+}
